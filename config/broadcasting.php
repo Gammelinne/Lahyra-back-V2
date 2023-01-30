d880a71b1e943b4eae1bfff98f2b0683e9c92e40
Renault Kylian <kylian.renault@viacesi.fr>
2023-01-30 22:23:18 +0100

    add socket libraire

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,11 +36,11 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => env('MODE_ENV') === 'production' ? 'https' : 'http',
+                'encrypted' => env('MODE_ENV') === 'production' ? true : false,
+                'useTLS' => env('MODE_ENV') === 'production' ? 'https' : 'http',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
